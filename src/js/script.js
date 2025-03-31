6d8470fb4bd572ad146ebacfc0787a4f90cfb54e
Eduardo <eduardodmoraes@icloud.com>
2025-03-31 09:08:01 -0300

    declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// declaraçao e variaveis 
+
+var nome ="fiap";
+console.log (nome)
+
+let idade ="19";
+console.log(idade)
+
+const sobrenome ="fiapinho";
+console.log (sobrenome)
+
+// declarando uma variavel indefinida
+let nome1;
+console.log(nome1)
+
+// declarando uma variavel nula
+let nome2 = null;
+console.log(nome2)
