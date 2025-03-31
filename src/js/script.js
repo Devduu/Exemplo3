c75100628235cf39bc088793b93750e54f36ed6b
Eduardo <eduardodmoraes@icloud.com>
2025-03-31 09:20:42 -0300

    conversao de tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,43 @@
 // declaraçao e variaveis 
-var nome ="fiap";
-console.log (nome)
+var nome = "fiap";
+console.log(nome)
 
-let idade ="19";
+let idade = "19";
 console.log(idade)
 
-const sobrenome ="fiapinho";
-console.log (sobrenome)
+const sobrenome = "fiapinho";
+console.log(sobrenome)
 
 // declarando uma variavel indefinida
 let nome1;
 console.log(nome1)
 
 // declarando uma variavel nula
-let nome2 =null;
+let nome2 = null;
 console.log(nome2)
 
 // tipos de variaveis
-let exeplo1 =10;
+let exeplo1 = 10;
 console.log(typeof exeplo1)
 
-let exemplo2 ="tecnologia";
+let exemplo2 = "tecnologia";
 console.log(typeof exemplo2)
 
-let exemplo3 =true;
+let exemplo3 = true;
 console.log(typeof exemplo3)
 
-let exemplo4 ={};
+let exemplo4 = {};
 console.log(typeof exemplo4)
 
-let exemplo5 =[];
+let exemplo5 = [];
 console.log(typeof exemplo5)
+
+// conversoes
+let numFloat = 123.456;
+console.log(parseInt(numFloat));
+
+let numString = "123.456"
+console.log(parseFloat(numString));
+
+
+
