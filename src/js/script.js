5bef6d0729d257a8b703a4dc9a505d8c91143d61
Eduardo <eduardodmoraes@icloud.com>
2025-03-31 09:13:44 -0300

    tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 // declaraçao e variaveis 
-
 var nome ="fiap";
 console.log (nome)
 
@@ -14,5 +13,21 @@ let nome1;
 console.log(nome1)
 
 // declarando uma variavel nula
-let nome2 = null;
+let nome2 =null;
 console.log(nome2)
+
+// tipos de variaveis
+let exeplo1 =10;
+console.log(typeof exeplo1)
+
+let exemplo2 ="tecnologia";
+console.log(typeof exemplo2)
+
+let exemplo3 =true;
+console.log(typeof exemplo3)
+
+let exemplo4 ={};
+console.log(typeof exemplo4)
+
+let exemplo5 =[];
+console.log(typeof exemplo5)
